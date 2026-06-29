071563cd7c0cc16611409121c0ed454a41e3bd41
Jorge2218 <jt4282863@gmail.com>
2026-06-28 21:32:01 -0500

    correccion al codigo dado a errores en otros dispositivos
    un espacio dio un error en el odoo de un compañero

---
+++ zapato_valoracion.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class ZapatosValoracion(models.Model):
+    _inherit = 'zapatos.zapato'
+
+    puntuacion = fields.Selection([
+        ('0', 'Normal'),
+        ('1', '⭐'),
+        ('2', '⭐⭐'),
+        ('3', '⭐⭐⭐'),
+    ], string='Puntuación', required=True)
+
+    imagen = fields.Image(string='Imagen', max_width=256, max_height=256, required=True)
